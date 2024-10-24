c5e4046a051bcc81bea1e0632c2b65f4a098869c
TristanKhadka11 <148723177+freekhadka@users.noreply.github.com>
2024-10-23 20:42:38 -0400

    v3

--- js/script.js
+++ js/script.js
@@ -2,4 +2,33 @@
 gsap.to("#mainTitle", { duration: 1, opacity: 1, y: -50 });
 gsap.to("p", { duration: 1, delay: 0.5, opacity: 1, y: -20 });
 
+  // GSAP Animation for main title
+  gsap.to("#mainTitle", {
+    duration: 1.5,
+    opacity: 1,
+    y: -50,
+    ease: "power2.out",
+    delay: 0.5
+});
+
+// Additional animations for the video section
+gsap.from(".video-section", {
+    duration: 1,
+    opacity: 0,
+    y: 30,
+    ease: "power2.out",
+    delay: 2
+});
+
+// Animation for text section
+gsap.from(".text-section", {
+    duration: 1,
+    opacity: 0,
+    y: 30,
+    ease: "power2.out",
+    delay: 2.5
+});
+
+
+
 
